fd7cfb8cf79a6df9ebcc3a9dbcfc88e590a95b56
Thomas Kjær <thomas@thomaskjaer.com>
2026-03-13 18:19:26 +0100

    Add etr v1.1.1

--- Formula/etr.rb
+++ Formula/etr.rb
@@ -1,8 +1,8 @@
 class Etr < Formula
   desc "ECMP traceroute tool with path detection and visualization support"
   homepage "https://github.com/tkjaer/etr"
-  url "https://github.com/tkjaer/etr/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "ff25a543f5028e63dedb9328eb9d44e6ce4c76d8632ccca8b0275c041cb408b7"
+  url "https://github.com/tkjaer/etr/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "e11f80d0da13d2067d4e9c04bf335d4cb3b6b079befaf0083953e193d3e5a1e3"
   license "MIT"
 
   depends_on "go" => :build
@@ -14,9 +14,7 @@ class Etr < Formula
 
     ldflags = [
       "-s", "-w",
-      "-X", "github.com/tkjaer/etr/internal/version.Version=#{version}",
-      "-X", "github.com/tkjaer/etr/internal/version.GitCommit=unknown",
-      "-X", "github.com/tkjaer/etr/internal/version.BuildDate=homebrew"
+      "-X", "github.com/tkjaer/etr/internal/version.Version=#{version}"
     ]
 
     system "go", "build", *std_go_args(output: bin/"etr", ldflags: ldflags.join(" ")), "./cmd/etr"
